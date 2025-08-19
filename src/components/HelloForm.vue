36cc0684b41c4e73dc969249633fc5306f48ba30
出井唯斗 <deiyuito@deiyuitonoMacBook-Pro.local>
2025-08-19 15:11:41 +0900

    vueサンプル入力フォーム作成

---
+++ src/components/HelloForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>名前を入力してください</h2>
+    <input v-model="name" placeholder="名前" />
+    <button @click="submit">送信</button>
+    <div v-if="submitted">
+      <h3>入力された名前リスト</h3>
+      <ul>
+        <li v-for="(user, index) in users" :key="index">{{ user }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+const name = ref('')
+const submitted = ref(false)
+const users = ref<string[]>([])
+const submit = () => {
+  if (name.value.trim() === '') return
+  users.value.push(name.value)
+  name.value = ''
+  submitted.value = true
+}
+</script>
+<style scoped>
+input {
+  margin-right: 10px;
+  padding: 4px 6px;
+}
+button {
+  padding: 4px 8px;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,6 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <RouterView />
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
